afe72d91ff2e985cfa3a72160509e7d12dfc3131
jstraume <joebarrystraume@gmail.com>
2014-09-22 14:52:40 -0700

    Edit file structure of project

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var express = require('express');
-// var bodyparser = require('body-parser');
-// var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
 var http = require('http');
 var app = express();
 
-// mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/users-development');
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/users-development');
 
 app.use(express.static(__dirname + (process.env.STATIC_DIR || '/build')));
 
-// app.use(bodyparser.json());
-// require('./routes/note-routes')(app);
+app.use(bodyparser.json());
+require('./routes/note-routes')(app);
 
 var server = http.createServer(app);
 
