d70689136224078c1ccb953890f42d89041add8d
macikokoro <betwinsouls@gmail.com>
2014-09-22 23:57:07 -0700

    fixing app.js and notes.html

--- app/js/app.js
+++ app/js/app.js
@@ -1,26 +1,26 @@
-<div data-ng-controller="notesController">
-  <button data-ng-click="deleteAll()">Sacrifice All to the MonGOD</button></br>
-  <form name="newnoteform">
-    <label data-ng-show="newnoteform.$dirty && newnoteform.$invalid">Note can't be blank</label>
-    <input type="text" required data-ng-model="newNote.noteBody">
-    <button data-ng-disabled="newnoteform.$invalid" data-ng-click="saveNewNote()">Save Note</button>
-  </form>
-  <div data-ng-repeat="note in notes">
-    <p>
-      <div data-ng-show="note.editing">
-        <form name="noteform">
-          <label data-ng-show="noteform.$invalid">Note can't be blank</label>
-          <input required data-ng-model="note.noteBody"/>
-          <button data-ng-disabled="noteform.$invalid" data-ng-click="saveNote(note)">Save Changes</button>
-          <button data-ng-click="cancelEdit(note, noteform)">Cancel</button>
-        </form>
-      </div>
+'use strict';
+//comment for changes
+require('angular/angular');
+require('angular-route');
 
-      <div data-ng-hide="note.editing">
-        {{note.noteBody | sentencify}}
-        <button data-ng-click="editNote(note)">Edit Note</button>
-        <button data-ng-click="deleteNote(note)">Delete Note</button>
-      </div>
-    </p>
-  </div>
-</div>  
+var notesApp = angular.module('notesApp', ['ngRoute']);
+
+//controlers
+require('./notes/controllers/notes-controller')(notesApp);
+
+//filters
+require('./filters/sentence-filter')(notesApp);
+
+//services
+require('./notes/services/notes-server')(notesApp);
+
+notesApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/notes', {
+      templateUrl: 'views/notes/notes.html',
+      controller: 'notesController'
+    })
+    .otherwise({
+      redirectTo: '/notes'
+    });
+}])
