a73449a6159bbbfaea299ca39cda9962e736b1a1
lwscroggins <larry.scroggins@gmail.com>
2014-09-24 10:09:23 -0700

    updated package.json with npm angular-d3js

--- app/js/dataviz/controllers/svg-controller.js
+++ app/js/dataviz/controllers/svg-controller.js
@@ -1,11 +1,11 @@
-'use strict'
+// 'use strict'
 
-module.exports = function(app) {
-  app.controller("svgController", function($scope, svgServer) {
-    $scope.makeSvg = function() {
-      d3.select("body").update("#svgArea").append("svg").attr("width", 900).attr("height", 500);
-    }
-  }
+// module.exports = function(app) {
+//   app.controller("svgController", function($scope, svgServer) {
+//     $scope.makeSvg = function() {
+//       d3.select("body").update("#svgArea").append("svg").attr("width", 900).attr("height", 500);
+//     }
+//   }
 
-  $scope.makeSvg();
-}
+//   $scope.makeSvg();
+// }
